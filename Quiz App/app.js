537a0d301ed314d20e735e384da63f2eef70b426
Irfan Khan <irfan.ik227@gmail.com>
2018-01-01 14:55:34 +0500

    Questions updated

--- Quiz App/app.js
+++ Quiz App/app.js
@@ -12,39 +12,93 @@ quesNumber.innerHTML = 'Q'+ quesCounter+':';
 let quesArray = [
     {
         id: 01,
-        question: 'which one is the capital city of pakistan',
-        option1: 'Karcahi',
-        option2: 'Lahore',
-        option3: 'Islamabad',
-        option4: 'Faisalabad',
-        correctAns: 'Islamabad'
+        question: 'Inside which HTML element do we put the JavaScript?',
+        option1: '<js>',
+        option2: '<script>',
+        option3: '<javascript>',
+        option4: '<scripting>',
+        correctAns: '<script>'
     },
     {
         id: 02,
-        question: 'which one is the biggest city of pakistan',
-        option1: 'Karcahi',
-        option2: 'Lahore',
-        option3: 'Islamabad',
-        option4: 'Faisalabad',
-        correctAns: 'Karachi'
+        question: 'Where is the correct place to insert a JavaScript?',
+        option1: 'The <head> section',
+        option2: 'Both the <head> section and the <body> section are correct',
+        option3: 'The <body> section',
+        option4: 'None of these',
+        correctAns: 'The <body> section'
     },
     {
         id: 03,
-        question: 'which one is the most advanced city of pakistan',
-        option1: 'Karcahi',
-        option2: 'Lahore',
-        option3: 'Islamabad',
-        option4: 'Faisalabad',
-        correctAns: 'Islamabad'
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        option1: '<script name="xxx.js">',
+        option2: '<script href="xxx.js">',
+        option3: '<script src="xxx.js">',
+        option4: 'None of these',
+        correctAns: '<script src="xxx.js">'
     },
     {
         id: 04,
-        question: 'HTML is a ........... language',
-        option1: 'Markup',
-        option2: 'Scripting',
-        option3: 'Programming',
-        option4: 'Interpreting',
-        correctAns: 'Markup'
+        question: 'The external JavaScript file must contain the <script> tag.',
+        option1: 'False',
+        option2: 'True',
+        option3: 'Depend on the file',
+        option4: 'None of these',
+        correctAns: 'False'
+    },
+    {
+        id: 05,
+        question: 'How do you write "Hello World" in an alert box?',
+        option1: 'alert("Hello World");',
+        option2: 'msg("Hello World");',
+        option3: 'alertBox("Hello World");',
+        option4: 'msgBox("Hello World");',
+        correctAns: 'alert("Hello World");'
+    },
+    {
+        id: 06,
+        question: 'How do you create a function in JavaScript?',
+        option1: 'function = myFunction()',
+        option2: 'function myFunction()',
+        option3: 'function:myFunction()',
+        option4: 'None of these',
+        correctAns: 'function myFunction()'
+    },
+    {
+        id: 07,
+        question: 'How do you call a function named "myFunction"?',
+        option1: 'myFunction()',
+        option2: 'call function myFunction()',
+        option3: 'call myFunction()',
+        option4: 'myFunction = ()',
+        correctAns: 'myFunction()'
+    },
+    {
+        id: 08,
+        question: 'How to write an IF statement in JavaScript?',
+        option1: 'if i == 5 then',
+        option2: 'if (i == 5)',
+        option3: 'if i = 5',
+        option4: 'if i = 5 then',
+        correctAns: 'if (i == 5)'
+    },
+    {
+        id: 09,
+        question: 'What is the correct way to write a JavaScript array?',
+        option1: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")',
+        option2: 'var colors = (1:"red", 2:"green", 3:"blue")',
+        option3: 'var colors = "red", "green", "blue"',
+        option4: 'var colors = ["red", "green", "blue"]',
+        correctAns: 'var colors = ["red", "green", "blue"]'
+    },
+    {
+        id: 10,
+        question: 'How do you round the number 7.25, to the nearest integer?',
+        option1: 'Math.round(7.25)',
+        option2: 'round(7.25)',
+        option3: 'rnd(7.25)',
+        option4: 'Math.rnd(7.25)',
+        correctAns: 'Math.round(7.25)'
     }
 ];
 
